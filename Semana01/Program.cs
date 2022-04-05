868aa601cac5a28ace183e187185571ee13db958
Steampunkyi <steampunkyi@outlook.com>
2022-04-05 11:14:37 -0500

    Configurando C#

---
+++ Semana01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Semana01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is my first program in C#");
+        }
+    }
+}
